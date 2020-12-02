b853015e2e9074d962dffae873e2c0814f2ac29f
Mark Garland <mgarland@soltra.com>
2020-12-01 20:23:06 -0500

    initial commit - day 1

---
+++ src/advent_of_code/2020/day01.clj
@@ -0,0 +1,35 @@
+(ns advent-of-code.2020.day01
+  (:require [clojure.math.combinatorics :as combo]))
+
+(def entries [1721 979 366 299 675 1456])
+
+(defn split-nums [nums-str]
+  (.split nums-str "\\s+"))
+
+(def input
+  (->> "resources/2020/day01.txt"
+       (slurp)
+       (split-nums)
+       (map #(Integer/parseInt %))))
+
+(map #(Long/parseLong ^String %) input)
+
+(defn make-groups [group-size entries]
+  (combo/combinations entries group-size))
+
+(defn find-group [total groups]
+  (->> groups
+       (filter (fn [[& entries]]
+                 (= total (apply + entries))))
+       first))
+
+(defn do-it [entries total group-size]
+  (->> entries
+       (make-groups group-size)
+       (find-group total)
+       (apply *)))
+
+;; part 1
+; (do-it input 2020 2)
+;; part 2
+; (do-it input 2020 3)
